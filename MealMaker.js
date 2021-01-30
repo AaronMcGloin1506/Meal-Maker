493e8e831b51e834eedbc2e977ba8f9b975f16a7
AaronMcGloin1506 <65114478+AaronMcGloin1506@users.noreply.github.com>
2021-01-30 17:20:08 +0000

    Update MealMaker.js

--- MealMaker.js
+++ MealMaker.js
@@ -1,76 +1,80 @@
 const menu = {
-    _courses: {
-      _appetizers: [],
-      _mains: [],
-      _desserts: [],
-      
-      get appetizers() {
-        return this._appetizers;
-      },
-      set appetizers(appetizersIn) {
-        this._appetizers = appetizersIn;
-      },
-      get mains() {
-        return this._mains;
-      },
-      set mains(mainsIn) {
-        this._mains = mainsIn;
-      },
-      get desserts() {
-        return this._desserts;
-      },
-      set desserts(dessertsIn) {
-        this._desserts = dessertsIn;
-      },
-    },
-    
-    get courses() {
-      return {
-        appetizers: this._courses.appetizers,
-        mains: this._courses.mains,
-        desserts: this._courses.desserts,
-      };
+  _courses: {
+    appetizers: [],
+    mains: [],
+    desserts: []
   },
+
+  get courses() {
+    return {
+      appetizers: this.appetizers,
+      mains: this.mains,
+      desserts: this.desserts,
+    }
+  },
+
+  get appetizers() {
+    return this._courses.appetizers
+  },
+  set appetizers (appetizersIn){
+    this._courses.appetizers.push(appetizersIn)
+  },
+  get mains() {
+    return this._courses.mains
+  },
+  set mains (mainsIn){
+    this._courses.mains.push(mainsIn)
+  },
+  get desserts() {
+    return this._courses.desserts
+  },
+  set desserts (dessertsIn){
+    this._courses.desserts.push(dessertsIn)
+  },
+
+  addDishToCourse (courseName, dishName, dishPrice) {
+    const dish = {
+      name: dishName,
+      price: dishPrice
+    };
+    //console.log(dish)
+
+    this._courses[courseName].push(dish)
+  },
+
+  getRandomDishFromCourse (courseName) {
+    const courseDishes = this.courses[courseName]
+    const randNum = Math.floor(Math.random()*courseDishes.length)
+    return courseDishes[randNum]
+  },
+
+  generateRandomMeal() {
+    const appetizer = menu.getRandomDishFromCourse('appetizers');
+    const mains = menu.getRandomDishFromCourse('mains');
+    const desserts = menu.getRandomDishFromCourse('desserts');
+
+    const price = appetizer.price + mains.price + desserts.price;
+
+    return {
+      appetizer:appetizer.name,
+      mains: mains.name,
+      dessert:desserts.name,
+      price: price
+    } 
     
-    ///===============================================================
-    
-    addDishToCourse (courseName, dishName, dishPrice) {
-      const dish = {
-        name: dishName,
-        price: dishPrice,
-      };
-      
-      this._courses[courseName].push(dish);
-    },
-    
-    getRandomDishFromCourse (courseName) {
-      const dishes = this._courses[courseName];
-      const randomIndex = Math.floor(Math.random() * dishes.length);
-      return dishes[randomIndex];
-    },
-    
-    generateRandomMeal() {
-      const appetizer = this.getRandomDishFromCourse('appetizers');
-      const main = this.getRandomDishFromCourse('mains');
-      const dessert = this.getRandomDishFromCourse('desserts');
-      const totalPrice = appetizer.price + main.price + dessert.price;
-      
-      return `Your meal is ${appetizer.name}, ${main.name} and ${dessert.name}. The price is $${totalPrice.toFixed(2)}.`;
-    },
-  };
-  
-  menu.addDishToCourse('appetizers', 'Ceasar Salad', 4.25);
-  menu.addDishToCourse('appetizers', 'Prawn Coctail', 4.25);
-  menu.addDishToCourse('appetizers', 'Garlic Bread', 3.50);
-  
-  menu.addDishToCourse('mains', 'Lasagna', 9.75);
-  menu.addDishToCourse('mains', 'Ribeye Steak', 14.95);
-  menu.addDishToCourse('mains', 'Fish & Chips', 12.95);
-  
-  menu.addDishToCourse('desserts', 'Cheese Cake', 4.50);
-  menu.addDishToCourse('desserts', 'Creme Brule', 4.25);
-  menu.addDishToCourse('desserts', 'Cheese Board', 3.25);
-  
-  let meal = menu.generateRandomMeal();
-  
-  console.log(meal);
+  },
+}
+
+menu.addDishToCourse('appetizers', 'CaesarSalad', 4.25);
+menu.addDishToCourse('appetizers', 'wings', 4.75);
+menu.addDishToCourse('appetizers', 'soup', 4.45);
+
+menu.addDishToCourse('mains', 'burger', 12.45);
+menu.addDishToCourse('mains', 'pasta', 14.25);
+menu.addDishToCourse('mains', 'steak', 24.25);
+
+menu.addDishToCourse('desserts', 'ice cream', 4.25);
+menu.addDishToCourse('desserts', 'apple tart', 4.25);
+menu.addDishToCourse('desserts', 'triffle', 4.25);
+
+console.log(menu.generateRandomMeal());
